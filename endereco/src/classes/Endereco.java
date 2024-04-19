1d98d56d4f2795aeddbf2a6201774799c64c867c
felipemoreira07 <moreiraf242@gmail.com>
2024-04-19 17:13:39 -0300

    adicionado loc, endereco e país

---
+++ endereco/src/classes/Endereco.java
@@ -0,0 +1,8 @@
+public class Endereco {
+    private TipoEndereco tipoEndereco;
+    private Integer numero;
+    private String complemento;
+    private Integer CEP;
+    private Logradouro logradouro;
+    private Bairro bairro;
+}
--- endereco/src/classes/Estado.java
+++ endereco/src/classes/Estado.java
@@ -1,4 +1,5 @@
 public class Estado {
     private String sigla;
     private String nome;
+    private Pais pais;
 }
